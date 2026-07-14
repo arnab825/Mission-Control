cb0b4f546ff68799468c3456ae755c5fd1f904ad
Arnab Roy <rarnab225@gmail.com>
2026-07-15 00:26:22 +0530

    chore: add hardware monitor build configuration and update application version metadata

--- Gaming/backend/system/hardware_monitor/obj/Release/net10.0/HardwareMonitor.AssemblyInfo.cs
+++ Gaming/backend/system/hardware_monitor/obj/Release/net10.0/HardwareMonitor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HardwareMonitor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f10527b70bc1d4303c62b923494fd23ae94cdfce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e78bca5be80f31c3eb546aba250c0eb624871dfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("HardwareMonitor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HardwareMonitor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
