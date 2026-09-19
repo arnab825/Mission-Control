5eb80ca6613493c8293e6af659c258418c06bd9c
Arnab Roy <rarnab225@gmail.com>
2026-09-19 23:58:09 +0530

    ci: configure clean Azure deployment and direct CDN download links

--- Gaming/website/src/lib/download.ts
+++ Gaming/website/src/lib/download.ts
@@ -1,11 +1,15 @@
-export const AUTO_DOWNLOAD_URL = "/api/download";
-export const WINDOWS_INSTALLER_URL = "/api/download?type=exe";
-export const WINDOWS_MSI_URL = "/api/download?type=msi";
-export const WINDOWS_ZIP_URL = "/api/download?type=zip";
+const GITHUB_REPO = "arnab825/Mission-Control";
+const GITHUB_BASE = `https://github.com/${GITHUB_REPO}/releases/latest/download`;
 
-export const LINUX_APPIMAGE_URL = "/api/download?type=AppImage";
-export const LINUX_DEB_URL = "/api/download?type=deb";
-export const LINUX_RPM_URL = "/api/download?type=rpm";
-export const LINUX_TAR_URL = "/api/download?type=tar.gz";
-export const LINUX_ZIP_URL = "/api/download?type=linux-zip";
-export const LINUX_INSTALLER_URL = "/api/download?type=linux";
+export const AUTO_DOWNLOAD_URL = `${GITHUB_BASE}/MissionControl-Setup.exe`;
+export const WINDOWS_INSTALLER_URL = `${GITHUB_BASE}/MissionControl-Setup.exe`;
+export const WINDOWS_MSI_URL = `${GITHUB_BASE}/MissionControl-Setup.msi`;
+export const WINDOWS_ZIP_URL = `${GITHUB_BASE}/MissionControl-Portable.zip`;
+
+export const LINUX_APPIMAGE_URL = `${GITHUB_BASE}/MissionControl-Linux.AppImage`;
+export const LINUX_DEB_URL = `${GITHUB_BASE}/MissionControl-Linux.deb`;
+export const LINUX_RPM_URL = `${GITHUB_BASE}/MissionControl-Linux.rpm`;
+export const LINUX_TAR_URL = `${GITHUB_BASE}/MissionControl-Linux.tar.gz`;
+export const LINUX_ZIP_URL = `${GITHUB_BASE}/MissionControl-Linux.zip`;
+export const LINUX_INSTALLER_URL = `${GITHUB_BASE}/MissionControl-Linux.AppImage`;
+
